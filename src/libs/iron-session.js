de746ba29adb230db1d42c0afa3d44913a94507a
oldhand7 <oldhanddev@gmail.com>
2022-04-09 09:23:29 +0300

    Rebuild with special cookie name

--- src/libs/iron-session.js
+++ src/libs/iron-session.js
@@ -4,7 +4,7 @@ import { withIronSessionSsr } from 'iron-session/next';
 // export const longTtl = 3600 * 24 * 30;
 
 export const sessionOptions = {
-    cookieName: "user",
+    cookieName: "__session",
     password: process.env.SESSION_SECRET,
     cookieOptions: {
       secure: process.env.APP_ENV === "production",
