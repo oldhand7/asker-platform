752810c6e7aefe1d30e6e7a2be4d51130b8ced1c
oldhand7 <oldhanddev@gmail.com>
2022-04-11 14:37:22 +0300

    Draggable stages.

--- src/components/FeatureDragDropLabel/FeatureDragDropLabel.js
+++ src/components/FeatureDragDropLabel/FeatureDragDropLabel.js
@@ -2,10 +2,11 @@ import classNames from 'classnames';
 
 import styles from './FeatureDragDropLabel.module.scss';
 
-const FeatureDragDropLabel = ({ className, feature, context = 'menu' }) => {
-  return <span className={classNames(styles['feature-drag-drop-label'], className, styles[`feature-drag-drop-label-${feature.type}`])}>
-    {feature.title}
-  </span>
+const FeatureDragDropLabel = ({ index = 0, onClick, className, feature, context = 'menu' }) => {
+
+  return <span onClick={onClick} className={classNames(styles['feature-drag-drop-label'], className, styles[`feature-drag-drop-label-${feature.type}`], styles[`feature-drag-drop-label-${context}`])}>
+    {feature.name}
+</span>
 }
 
 export default FeatureDragDropLabel;
